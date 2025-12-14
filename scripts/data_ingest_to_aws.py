9d7c68a99827025281078c4d501ad8a385e9d499
AlexKurinets <alex.kurinets@gmail.com>
2025-12-14 16:33:12 -0500

    code to persist bet data to aws s3

---
+++ scripts/data_ingest_to_aws.py
@@ -0,0 +1,51 @@
+import json
+import boto3
+from botocore.exceptions import ClientError
+import pandas as pd
+import requests
+from io import StringIO
+
+# Get secrets
+secrets_client = boto3.client('secretsmanager', region_name='us-east-2')
+secret_name = 'secrets_key'
+response = secrets_client.get_secret_value(SecretId=secret_name)
+secrets = json.loads(response['SecretString'])
+aws_access_key = secrets['aws_access_key_id']
+aws_secret_key = secrets['aws_secret_access_key']
+s3_bucket = secrets['bucket_name']
+
+# Download Google Sheet as CSV
+sheet_id = '1wM00QhayythUnOizO7hIzv4hDHQutmTS'
+gid = '1467560424'
+export_url = f'https://docs.google.com/spreadsheets/d/{sheet_id}/export?format=csv&gid={gid}'
+response = requests.get(export_url)
+csv_content = response.content.decode('utf-8')
+df_new = pd.read_csv(StringIO(csv_content))
+df_new['Date'] = pd.to_datetime(df_new['Date'], format='%m/%d/%Y')
+
+# S3 setup
+s3 = boto3.client('s3', aws_access_key_id=aws_access_key, aws_secret_access_key=aws_secret_key)
+s3_key = 'bet_data.csv'
+
+# Get existing data
+try:
+    obj = s3.get_object(Bucket=s3_bucket, Key=s3_key)
+    df_existing = pd.read_csv(obj['Body'])
+    max_date = df_existing['Date'].max()
+    df_append = df_new[df_new['Date'] > max_date]
+except ClientError as e:
+    if e.response['Error']['Code'] == 'NoSuchKey':
+        df_append = df_new
+        df_existing = pd.DataFrame(columns=df_new.columns)
+    else:
+        raise
+
+# Upload new records
+if not df_append.empty:
+    df_updated = pd.concat([df_existing, df_append], ignore_index=True)
+    csv_buffer = StringIO()
+    df_updated.to_csv(csv_buffer, index=False)
+    s3.put_object(Bucket=s3_bucket, Key=s3_key, Body=csv_buffer.getvalue())
+    print(f"Loaded {len(df_append)} new records.")
+else:
+    print("No new records.")
