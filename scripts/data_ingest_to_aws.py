ba1d6dac503c8357a6560b4a043ee2c500d89cc4
AlexKurinets <alex.kurinets@gmail.com>
2025-12-24 09:19:32 -0500

    training script aligns with 'scoring_focus' training script in all ways except parallel processing

--- scripts/data_ingest_to_aws.py
+++ scripts/data_ingest_to_aws.py
@@ -25,7 +25,7 @@ df_new['Date'] = pd.to_datetime(df_new['Date'], format='%m/%d/%Y')
 
 # S3 setup
 s3 = boto3.client('s3', aws_access_key_id=aws_access_key, aws_secret_access_key=aws_secret_key)
-s3_key = 'bet_data.csv'
+s3_key = 'bet_data/bet_data.csv'
 
 # Get existing data
 try:
